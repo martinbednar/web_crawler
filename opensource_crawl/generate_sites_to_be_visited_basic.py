2e3cc3e129d3a1be3a500b5def2d3ab87449aede
martinbednar <martin.bedn20@gmail.com>
2021-11-10 12:38:30 +0100

    Add a script for basic generating sites to be visited.

---
+++ opensource_crawl/generate_sites_to_be_visited_basic.py
@@ -0,0 +1,16 @@
+output = '{"sites": ['
+
+i = 1
+
+with open("opensource_homepages") as file:
+    for line in file:
+    	line = line.rstrip()
+    	output += '{"site_url": "' + line + '", "links_count": 1, "links": ["' + line + '"], "tranco_rating": "' + str(i) + '"}, '
+    	i += 1
+
+output = output[:-2]
+output += ']}'
+
+with open('oupensource_sites_to_be_visited.json', 'w') as f:
+    f.write(output)
+
